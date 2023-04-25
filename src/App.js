7822ca1fa5ddd4feab3a77b99f3c25ba2c4af1ff
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-04-25 14:16:03 +0530

    created useRef hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import {useState,createContext} from 'react'
+import {useState,createContext,useEffect,useRef} from 'react'
 import ComA from './Components/ComA';
 
 const AppState = createContext();
 function App() {
+
+  const[input,setInput]=useState("")
+
+  const counter = useRef(0);
+  useEffect(()=>{
+    counter.current=counter.current+1;
+  })
+
+  const formHandler=(e)=>{
+    setInput(e.target.value)
+  }
 
   const[data, setData] = useState("hello Ark")
   const[PersonalDetail,setDetail]=useState({name:"vivek",age:"22"})
@@ -12,6 +23,8 @@ function App() {
       <AppState.Provider value={{data,PersonalDetail}}>
       <ComA/>
       </AppState.Provider>
+      <input type={Text} onChange={formHandler}/>
+      <h2>{counter.current}</h2>
     </div>
   );
 }
